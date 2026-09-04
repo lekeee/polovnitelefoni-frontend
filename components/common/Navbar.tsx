1a8e76ddc79475be7d29d9ff31f57c28cb4c5df9
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2026-09-04 20:17:08 +0200

    Enhance Navbar with UserAvatar and profile details

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -26,12 +26,15 @@ export default function Navbar() {
             <div>
                 <input type="text" placeholder="Search..." className="border border-gray-300 rounded-md px-4 py-2 w-64" />
             </div>
-            <div className="flex">
-                <div>
+            <Link href="/login" className="flex">
+                <div className="flex items-center gap-3">
                     <UserAvatar />
-
+                    <div className="flex flex-col items-start">
+                        <span className="text-sm text-secondary leading-tight">Prijavi se na</span>
+                        <span className="text-lg font-bold leading-tight">Profil</span>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
-    </nav>
+    </nav >
 }
